e97ec6c79cb9f25d517790a4c1ec680e54eec78e
stiv19 <stevencas0002@gmail.com>
2025-02-24 11:25:27 -0500

    Archivo persona V6.3 (Se modifica la clase persona, haciendo que sus atributos sean privados, ademas se crea los metodos set y get de esta clase)

--- Persona.py
+++ Persona.py
@@ -1,10 +1,29 @@
 class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
+    def __init__(self, nombre, edad, direccion):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__direccion=direccion
+    
+    def get_nombre(self):
+        return self.__nombre
+    
+    def set_nombre(self, nombre):
+        self.__nombre=nombre
+
+    def get_edad(self):
+        return self.__edad
+    
+    def set_edad(self, edad):
+        self.__edad=edad
+    
+    def get_direccion(self):
+        return self.__direccion
+    
+    def set_direccion(self, direccion):
+        self.__direccion=direccion
 
     def __str__(self):
-        return f"Nombre: {self.nombre}, Edad: {self.edad}"
+        return f"Nombre: {self.set_nombre}, Edad: {self.set_edad}, Direccion: {self.set_direccion}"
 
 # Clase Estudiante hereda de Persona
 class Estudiante(Persona):
