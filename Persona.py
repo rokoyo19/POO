4e03e50d9185f48687e5d1f4e9ebfede046c59ed
stiv19 <stevencas0002@gmail.com>
2025-02-24 10:36:43 -0500

    Archivo persona V6 (Se crea la clase docente que hereda los datos Nombre y edad de la clase persona)

--- Persona.py
+++ Persona.py
@@ -14,14 +14,11 @@ class Estudiante(Persona):
 
     def __str__(self):
         return f"{super().__str__()}, Carrera: {self.carrera}"
+    
+class Docente(Persona):
+    def __init__(self, nombre, edad, tipoContrato):
+        super().__init__(nombre, edad)  # Llamamos al constructor de la clase base (Persona)
+        self.tipoContrato = tipoContrato  # Atributo específico de Docente
 
-# Pedir datos al usuario
-nombre = input("Ingresa el nombre: ")
-edad = int(input("Ingresa la edad: "))
-carrera = input("Ingresa la carrera del estudiante: ")
-
-# Crear un objeto Estudiante
-estudiante = Estudiante(nombre, edad, carrera)
-
-# Mostrar los datos del estudiante
-print(estudiante)
+    def __str__(self):
+        return f"{super().__str__()}, Carrera: {self.carrera}"
